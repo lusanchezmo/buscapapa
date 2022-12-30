331a0835579c02372b6856573cb8e70f3a603d56
Luis <lusanchezmo@unal.edu.co>
2022-12-30 16:17:29 -0500

    Se fusiono el menu en desktop del correo usuario

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function fn_index_register(){
 function fn_index_login(){
     location.href="login.html";
 }
+
+console.log('dasdas')
+
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
