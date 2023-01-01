34de0b8c0ab3c74b348c8b20e30a08aa19d4d704
Luis <lusanchezmo@unal.edu.co>
2023-01-01 16:40:27 -0500

    Se solucionó el problema al abrir los menus anteriormente integrados

--- script.js
+++ script.js
@@ -12,21 +12,26 @@ function fn_index_login(){
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
-}
-
-
-
 //Para el carrito de compras
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
 carritoIcon.addEventListener('click', toggleCarritoAside);
 
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
 
 function toggleCarritoAside() {
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+    if(!isDesktopMenuClose){
+        desktopMenu.classList.add('inactive');
+    }
     aside.classList.toggle('inactive');
 }
