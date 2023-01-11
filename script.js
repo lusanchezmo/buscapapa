0f2f5b1e84ad8d57aa261d505c20dac8e72fb117
Luis <lusanchezmo@unal.edu.co>
2023-01-11 14:58:12 -0500

    Se agregaron los demás articulos de la tienda de papas.

--- script.js
+++ script.js
@@ -40,18 +40,66 @@ function toggleCarritoAside() {
 const productList = []
 productList.push({
     name: 'Papa pastusa',
-    price: 80000,
-    imagen: './assets/bulto.png',
+    price: 125000,
+    imagen: 'https://organicosysaludables.com/wp-content/uploads/2019/04/Papa-pastusa-organica.jpg',
 });
 productList.push({
-    name: 'Papa pastusa',
-    price: 80000,
-    imagen: './assets/bulto.png',
+    name: 'Papa Diacol capiro',
+    price: 160000,
+    imagen: 'https://www.eje21.com.co/site/wp-content/uploads/2020/10/papas-mercaldas.png',
 });
 productList.push({
-    name: 'Papa pastusa',
+    name: 'Papa criolla',
     price: 80000,
-    imagen: './assets/bulto.png',
+    imagen: 'https://vaquitaexpress.com.co/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/3/234.jpg',
+});
+
+productList.push({
+    name: 'Papa Tocarreña',
+    price: 210000,
+    imagen: 'https://wamifu.com/wp-content/uploads/papa-sabanera-1-600x600.png',
+});
+productList.push({
+    name: 'Papa Nevada',
+    price: 160000,
+    imagen: 'https://carulla.vtexassets.com/arquivos/ids/5943052/PAPA-NEVADA-UNIDAD-1601988_a.jpg?v=637672555669300000',
+});
+productList.push({
+    name: 'Papa suprema',
+    price: 125000,
+    imagen: 'https://selecpacol.com/img/papa/2.png',
+});
+
+productList.push({
+    name: 'Papa -Unica',
+    price: 110000,
+    imagen: 'https://s.cornershopapp.com/product-images/4452695.jpg?versionId=.8oh9khbgUxXr2Paf26ZJWiH6dnv6qgI',
+});
+productList.push({
+    name: 'Papa Icahuila',
+    price: 180000,
+    imagen: 'https://www.eltiempo.com/contenido-dj/historias/IMAGEN/IMAGEN-16882516-2.jpg',
+});
+productList.push({
+    name: 'Papa pastusa-superior',
+    price: 125000,
+    imagen: 'https://lagranjamarket.com.co/wp-content/uploads/2021/07/PAPAPASTUSA.jpg',
+});
+
+productList.push({
+    name: 'Papa Tocarreña',
+    price: 210000,
+    imagen: 'https://wamifu.com/wp-content/uploads/papa-sabanera-1-600x600.png',
+});
+productList.push({
+    name: 'Papa Nevada',
+    price: 160000,
+    imagen: 'https://carulla.vtexassets.com/arquivos/ids/5943052/PAPA-NEVADA-UNIDAD-1601988_a.jpg?v=637672555669300000',
+});
+productList.push({
+    name: 'Papa suprema',
+    price: 125000,
+    imagen: 'https://www.papaunc.com/sites/default/files/img_upload/Web_Suprema_tub2.JPG',
 });
 
 for (product of productList){
